0ef49a9bfb686e4dbe7bafbb6e1c3a964722579e
CPPBS <62123834+CPPBS@users.noreply.github.com>
2026-06-29 18:27:49 +0800

    Add files via upload

---
+++ mkv.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%*"=="" exit /b 0
+
+for %%i in (%*) do (
+    set "filepath=%cd%\%%i.vue"
+    set "filedir=%%~dpi"
+
+    if not "!filedir!"=="" (
+        if not exist "!filedir!" mkdir "!filedir!" 2>nul
+    )
+
+    (   
+        echo ^<script setup^>
+        echo ^</script^>
+        echo.
+        echo ^<template^>
+        echo   ^<h1^>You did it!^</h1^>
+        echo ^</template^>
+        echo.
+        echo ^<style scoped^>
+        echo ^</style^>
+    ) > "!filepath!"
+)
